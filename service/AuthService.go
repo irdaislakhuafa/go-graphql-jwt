a6373cb51898438a66dd70df660914a0268ab992
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-21 02:15:30 -0700

    add method for user login

--- service/AuthService.go
+++ service/AuthService.go
@@ -2,14 +2,18 @@ package service
 
 import (
 	"context"
+	"log"
 
 	"github.com/irdaislakhuafa/go-graphql-jwt/graph/model"
+	"github.com/irdaislakhuafa/go-graphql-jwt/tools"
 	"gorm.io/gorm"
 )
 
 func UserRegister(ctx context.Context, newUser model.NewUser) (interface{}, error) {
 	_, err := UserGetByEmail(ctx, newUser.Email)
 	if err == gorm.ErrRecordNotFound {
+		return nil, err
+	} else if err != nil {
 		return nil, err
 	}
 
@@ -25,6 +29,33 @@ func UserRegister(ctx context.Context, newUser model.NewUser) (interface{}, erro
 
 	response := map[string]interface{}{
 		"token": token,
+	}
+
+	return response, nil
+}
+
+func UserLogin(ctx context.Context, email string, password string) (interface{}, error) {
+	user, err := UserGetByEmail(ctx, email)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			log.Println("User with email:", email, "not found")
+			return nil, err
+		}
+		return nil, err
+	}
+
+	log.Println("Verify password")
+	if err := tools.VerifyPassword(user.Password, password); err != nil {
+		log.Println("Password not valid")
+		return nil, err
+	}
+	generatedToken, err := JwtGenerate(ctx, user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	response := map[string]interface{}{
+		"token": generatedToken,
 	}
 
 	return response, nil
