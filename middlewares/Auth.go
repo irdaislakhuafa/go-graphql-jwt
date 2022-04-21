c0001533ea8a679f84703d32ea553d2a0f41a4f5
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-21 04:21:38 -0700

    add auth middleware

---
+++ middlewares/Auth.go
@@ -0,0 +1,37 @@
+package middlewares
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/irdaislakhuafa/go-graphql-jwt/service"
+)
+
+type authString string
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	handlerFunc := func(writer http.ResponseWriter, request *http.Request) {
+		auth := request.Header.Get("Authorization")
+		if strings.Trim(auth, " ") == "" {
+			next.ServeHTTP(writer, request)
+			return
+		}
+
+		bearer := "Bearer "
+		token := auth[len(bearer):]
+
+		validate, err := service.JwtValidate(context.Background(), token)
+		if err != nil || !validate.Valid {
+			http.Error(writer, "Invalid token", http.StatusForbidden)
+			return
+		}
+
+		claims, _ := validate.Claims.(*service.JwtCustomClaims)
+		ctx := context.WithValue(request.Context(), authString("auth"), claims)
+		request = request.WithContext(ctx)
+		next.ServeHTTP(writer, request)
+	}
+
+	return http.HandlerFunc(handlerFunc)
+}
