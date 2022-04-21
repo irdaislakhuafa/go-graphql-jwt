37d713fa7c46a2d132f3ccdefe342b32fbcbded5
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-21 01:19:43 -0700

    rename EncryptPassword to EncodePassword and add function to verify password

--- tools/BCrypt.go
+++ tools/BCrypt.go
@@ -6,13 +6,18 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func EncryptPassword(password string) (string, error) {
+func EncodePassword(password string) (string, error) {
 	log.Println("Ecrypting password")
-	encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	encode, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		log.Println("Failed when encrypt password:", err.Error())
 		return "", err
 	}
 	log.Println("Success encrypt password")
-	return string(encryptedPassword), nil
+	return string(encode), nil
+}
+
+func VerifyPassword(encodedPassword string, realPassword string) error {
+	errorStatus := bcrypt.CompareHashAndPassword([]byte(encodedPassword), []byte(realPassword))
+	return errorStatus
 }
